05a19dc3a39b925274ad2c95e3a6ff4d356e39ed
ctrostel <trostel.connor@gmail.com>
2021-05-19 19:33:57 -0400

    state machine

---
+++ scripts/state.gd
@@ -0,0 +1,19 @@
+class_name State
+extends Node
+
+var state_machine = null
+
+func _handle_input(_event: InputEvent) -> void:
+	pass
+
+func update(_delta: float) -> void:
+	pass
+
+func _physics_update(_delta: float) -> void:
+	pass
+
+func _enter(_msg = {}) -> void:
+	pass
+
+func _exit() -> void:
+	pass
